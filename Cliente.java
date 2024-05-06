907f2a68298661cf6c404d0c5fe6fdbcd3c0fe48
thiagoc10 <161757954+thiagoc10@users.noreply.github.com>
2024-05-06 13:24:03 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,30 @@
+public class Cliente {
+
+    private String nome;
+    private String endereco;
+
+    public Cliente(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public void imprimir() {
+        System.out.println("Cliente inválido");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
